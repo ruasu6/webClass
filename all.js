65e0d6c4caed2a9e460ae42c8110f72e676cb51d
Takuniii <jieni0910@gmail.com>
2023-12-02 13:35:31 +0800

    new file

---
+++ all.js
@@ -0,0 +1,9 @@
+const content = document.getElementById('content');
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+const addedBtn = document.getElementById('addedBtn');
+const deletedBtn = document.getElementById('deletedBtn');
+
+addedBtn.addEventListener('click', function () {
+    
+})
